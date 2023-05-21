521e2a6809e2957de9e99c82e8a905ab9ea08a69
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-05-22 00:47:25 +0530

    Adding ExpenseItem with forEach loop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
 import ExpenseItem from "./Components/ExpenseItem";
+
 function App() {
+  const expense = [
+    {
+      title: "Food",
+      LocationOfExpenditure: "Indore",
+      Amount: 500,
+    },
+
+    {
+      title: "Travel",
+      LocationOfExpenditure: "Mumbai",
+      Amount: 650,
+    },
+    {
+      title: "Movie",
+      LocationOfExpenditure: "Bhopal",
+      Amount: 800,
+    },
+    {
+      title: "Shopping",
+      LocationOfExpenditure: "Bangluru",
+      Amount: 5600,
+    },
+    {
+      title: "Party Venue",
+      LocationOfExpenditure: "Delhi",
+      Amount: 8800,
+    },
+  ];
+
+
+
+  const expenseItems = []; // Create an empty array to hold the ExpenseItem components
+
+  expense.forEach((expense) => {
+    expenseItems.push(
+      <ExpenseItem
+        title={expense.title}
+        location={expense.LocationOfExpenditure}
+        amount={expense.Amount}
+      ></ExpenseItem>
+    );
+  });
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <div>
+        <h2>Let's get started!</h2>
+      </div>
+       {expenseItems}
+      {/* <ExpenseItem
+        title={expense[1].title}
+        location={expense[1].LocationOfExpenditure}
+        amount={expense[1].Amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[2].title}
+        location={expense[2].LocationOfExpenditure}
+        amount={expense[2].Amount}
+      ></ExpenseItem> */}
     </div>
   );
 }
