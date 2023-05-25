8b266e7a619b94fb0bdaaa010733f2b62461b9f5
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-05-25 15:42:44 +0530

    Add Expense of Rs.100 per click

--- src/Components/Expense/ExpenseItem.js
+++ src/Components/Expense/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React , { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import ExpenseDetails from './ExpenseDetails';
@@ -5,14 +6,28 @@ import './ExpenseItem.css'
 
 
 const ExpenseItem = (props) => {
+ 
+ 
+  const [amount , setAmount] = useState(props.amount);
 
-  const deleteHandler = (e) => {
+  const AddExpense = () => {
+    setAmount(amount + 100);
+  }
+ 
+  // const [title , setTitle] = useState(props.title);
+
+  // const changeTitle = () => {
+  //   setTitle("updated");
+  //  console.log(title);
+  // }
+
+  // const deleteHandler = (e) => {
     
-    let item = e.target.parentElement;
-    //console.log(item); It will target the whole expense item component and in next line we are deleting it.
-    item.remove();
+  //   let item = e.target.parentElement;
+  //   //console.log(item); It will target the whole expense item component and in next line we are deleting it.
+  //   item.remove();
     
-    }
+  //   }
 
   
    
@@ -22,9 +37,9 @@ const ExpenseItem = (props) => {
 
   <ExpenseDate date ={props.date} />
   
-  <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
-  
-  <button  onClick={deleteHandler} > Delete Expense </button>
+  <ExpenseDetails amount={amount}  location={props.location}   title={props.title} />
+  <button onClick = {AddExpense} > Add $100</button>
+  {/* <button  onClick={deleteHandler} > Delete Expense </button> */}
   </Card>
    
   );
