9585a1ad67c7ab5c90a3486a528004f03af862d9
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-05-25 11:08:34 +0530

    Adding button to delete expense component

--- src/Components/Expense/ExpenseItem.js
+++ src/Components/Expense/ExpenseItem.js
@@ -6,7 +6,16 @@ import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
 
+  const deleteHandler = (e) => {
+    
+    let item = e.target.parentElement;
+    //console.log(item); It will target the whole expense item component and in next line we are deleting it.
+    item.remove();
+    
+    }
+
   
+   
  return (
 
   <Card className='expense-item'>
@@ -15,6 +24,7 @@ const ExpenseItem = (props) => {
   
   <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
   
+  <button  onClick={deleteHandler} > Delete Expense </button>
   </Card>
    
   );
