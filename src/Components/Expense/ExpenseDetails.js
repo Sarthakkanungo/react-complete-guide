128b1f1920d060557d2ecebdb650dbedfbf687d0
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-05-25 15:45:48 +0530

    $ 100 increment per click

--- src/Components/Expense/ExpenseDetails.js
+++ src/Components/Expense/ExpenseDetails.js
@@ -5,7 +5,7 @@ const ExpenseDetails = (props) => {
         <div className='expense-item__description'>
         <h2> { props.title} </h2>
         <h2> { props.location}</h2>
-        <div className='expense-item__price'>RS.{props.amount}</div>
+        <div className='expense-item__price'>$.{props.amount}</div>
         </div>
  )
 }
